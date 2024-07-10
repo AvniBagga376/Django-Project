583e9d74f85ef1a9d85168c87e6cf2f70789eeef
AvniBagga376 <baggaavni@gmail.com>
2024-07-10 17:34:27 +0530

    Webpages added

---
+++ home/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Category, Product
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Category Name'}),
+        }
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'category', 'description', 'price']
--- home/models.py
+++ home/models.py
@@ -11,3 +11,18 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
