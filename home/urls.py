6aad146e6cc05dc06b2e1b5a0b24472a7c464553
AvniBagga376 <baggaavni@gmail.com>
2024-07-22 22:21:44 +0530

    Inventory

--- home/urls.py
+++ home/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path("categories/", views.categories, name='categories'),
     path("categories/delete/<int:category_id>/", views.delete_category, name='delete_category'),
     path("products/", views.products, name='products'),
-    path("delete_product/<int:product_id>/", views.delete_product, name='delete_product')
+    path("delete_product/<int:product_id>/", views.delete_product, name='delete_product'),
+    path("inventory/", views.inventory, name='inventory'),
+    path("inventory/<int:product_id>/", views.product_view, name='product_view'),
+    path("inventory/<int:history_id>/edit/", views.stock_history_edit, name='stock_history_edit'),
 ]
